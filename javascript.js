ba6dcdd629082ca63690485f8946615070e417f6
Daniel Kapper <dkapper01@gmail.com>
2017-12-04 14:48:48 -0500

    input and reverse string

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
-var submit = document.getElementById('submit').value; 
+console.log('working....'); 
 
 
 function submitText() {
-    console.log('red');
+    var submitText = document.getElementById('inputText').value;
+    var toArr = submitText.split(''); 
+    
+    for(var i = 0; i < toArr.length / 2; i++) {
+        var tempVar = toArr[i]; 
+        toArr[i] = toArr[toArr.length - 1 - i]; 
+        toArr[toArr.length - 1 - i] = tempVar; 
+    } 
+    var answer = toArr.join(''); 
+   document.getElementById('innterText').innerHTML = answer; 
 }
 
-console.log("blue")
+
