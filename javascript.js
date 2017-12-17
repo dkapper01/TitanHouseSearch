6cf1d1740dcc4d97fd56aaaeb003e229814ad9e2
Daniel Kapper <dkapper01@gmail.com>
2017-12-16 23:17:22 -0500

    add mean and part of median

--- javascript.js
+++ javascript.js
@@ -14,4 +14,29 @@ function submitText() {
    document.getElementById('innterText').innerHTML = answer; 
 }
 
+function meanMedianMode(array) {
+    return {
+        mean: getMean(array),
+        median: getMedian(array)
+        // modo: getMode
+    };
+}
+function getMean(array) {
+    var num=0; 
+    var mean=0; 
+    var mean; 
+    array.forEach(number => {
+        num += number; 
+    });
+    mean = num / array.length; 
+    console.log(mean); 
+    return
+}
 
+function getMedian(array) {
+    var median = 0; 
+    for(var i = 0; i < array.length / 2; i++) {
+        median = array[i]; 
+    }
+    console.log(median); 
+}
