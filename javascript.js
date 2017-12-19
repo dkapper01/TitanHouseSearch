a1c3a049b7b98b3c98e2f571b36d55d7809914d3
Daniel Kapper <dkapper01@gmail.com>
2017-12-19 16:11:13 -0500

    Working on search page

--- javascript.js
+++ javascript.js
@@ -1,85 +1,13 @@
 console.log('working....'); 
 
+var getCompany; 
 
-
-
-
-document.getElementById('searchBtn').addEventListener("click", function() {
-    var searchBox = document.getElementById('searchBox').value; 
-    document.querySelector(".putText").innerHTML = searchBox;
-}); 
-
-
-
-
-
-function callCat() {
-    document.querySelector(".cat").innerHTML = "dog"; 
+function submetCompany() {
+    getCompany = document.getElementById("company").value; 
+    document.querySelector(".linksOne").innerHTML = "President AND " + getCompany; 
+    document.querySelector(".linksTwo").innerHTML = "CEO AND " + getCompany;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function submitText() {
-//     var submitText = document.getElementById('inputText').value;
-//     var toArr = submitText.split(''); 
-    
-//     for(var i = 0; i < toArr.length / 2; i++) {
-//         var tempVar = toArr[i]; 
-//         toArr[i] = toArr[toArr.length - 1 - i]; 
-//         toArr[toArr.length - 1 - i] = tempVar; 
-//     } 
-//     var answer = toArr.join(''); 
-//    document.querySelector('.innterText').innerHTML = answer; 
-// }
-
-// function reverseText() {
-//     document.getElementById('inputText').value = ''; 
-//     document.getElementById('inputText').placeholder = "Reverse Text";  
-// }
-
-
-// function meanMedianMode(array) {
-//     document.getElementById('inputText').value = ''; 
-//     document.getElementById('inputText').placeholder = "Mean Median Mode";  
-//     return {
-//         mean: getMean(array),
-//         median: getMedian(array)
-//         // modo: getMode
-//     };
-// }
-// function getMean(array) {
-//     var num = 0; 
-//     var mean = 0; 
-//     var mean = 0; 
-//     array.forEach(number => {
-//         num += number; 
-//     });
-//     mean = num / array.length; 
-//     console.log(mean); 
-//     return
-// }
-
-// function getMedian(array) {
-//     var median = 0; 
-//     for(var i = 0; i < array.length / 2; i++) {
-//         median = array[i]; 
-//     }
-//     console.log(median); 
-// }
+function showHide() {
+    var getList = document.getElementById("toggleUl"); 
+}
