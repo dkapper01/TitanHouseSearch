0026d7631588b373f3ebeb1058bcd440973e6f35
Daniel Kapper <dkapper01@gmail.com>
2017-12-19 21:02:44 -0500

    quick link

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
 console.log('working....'); 
-
 var getCompany; 
 
-function submetCompany() {
-    getCompany = document.getElementById("company").value; 
-    document.querySelector(".linksOne").innerHTML = "President AND " + getCompany; 
-    document.querySelector(".linksTwo").innerHTML = "CEO AND " + getCompany;
-}
+document.getElementById("companyEnter").addEventListener('keydown', function(e) {
+    if(e.keyCode == 13) {
+        submetCompany(); 
+    }
+}); 
 
-function showHide() {
-    var getList = document.getElementById("toggleUl"); 
+
+function submetCompany() {
+    var getCompany = document.getElementById("companyEnter").value; 
+    document.querySelector(".links1").innerHTML = "President AND " + "&quot" + getCompany + "&quot"; 
+    document.querySelector(".links2").innerHTML = "Vice President AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links3").innerHTML = "CEO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links4").innerHTML = "CFO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links5").innerHTML = "COO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links6").innerHTML = "CMO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links7").innerHTML = "CMO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links8").innerHTML = "CLO AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links9").innerHTML = "site:www.businesswire.com AND " + "&quot" + getCompany + "&quot" + " AND Press Release";
 }
