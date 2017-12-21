2f2a42354a8defcf9ab665405a6ecf69d1e1a327
Daniel Kapper <dkapper01@gmail.com>
2017-12-20 22:35:36 -0500

    spacing

--- javascript.js
+++ javascript.js
@@ -16,10 +16,10 @@ function submetCompany() {
     document.querySelector(".links1").innerHTML = "&quot" + "President" + "&quot" + " AND " + "&quot" + getCompany + "&quot"; 
     document.querySelector(".links2").innerHTML = "&quot" + "Vice President" + "&quot" + " AND " + "&quot" + getCompany + "&quot";  
     document.querySelector(".links3").innerHTML = "&quot" + "CEO" + "&quot" + " AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links4").innerHTML = "&quot" + "CFO AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links5").innerHTML = "&quot" + "COO AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links6").innerHTML = "&quot" + "CMO AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links7").innerHTML = "&quot" +  " AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links8").innerHTML = "&quot" + "CLO AND " + "&quot" + getCompany + "&quot";
-    document.querySelector(".links9").innerHTML = "&quot" + "site:www.businesswire.com AND " + "&quot" + getCompany + "&quot" + " AND " + "&quot" + getExecutive + "&quot";
+    document.querySelector(".links4").innerHTML = "&quot" + "CFO" + "&quot" + " AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links5").innerHTML = "&quot" + "COO AND" + "&quot" + getCompany + "&quot";
+    document.querySelector(".links6").innerHTML = "&quot" + "CMO" + "&quot" + " AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links7").innerHTML = "&quot" + getCompany + "&quot" +  " AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links8").innerHTML = "&quot" + "CLO" + "&quot" + " AND " + "&quot" + getCompany + "&quot";
+    document.querySelector(".links9").innerHTML = "&quot" + "site:www.businesswire.com" + "&quot" + " AND " + "&quot" + getCompany + "&quot" + " AND " + "&quot" + getExecutive + "&quot";
 }
