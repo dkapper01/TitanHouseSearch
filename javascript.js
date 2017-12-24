65544ba4069d2483ca233b68660c1bbba0c18abf
Daniel Kapper <dkapper01@gmail.com>
2017-12-23 19:13:41 -0500

    Done!

--- javascript.js
+++ javascript.js
@@ -1,27 +1,23 @@
 console.log('working....'); 
-var getCompany; 
-
-document.getElementById("companyEnter").addEventListener('keydown', function(e) {
-    if(e.keyCode == 13) {
-        submetCompany(); 
-    }
-}); 
-
 
 function submetCompany() {
+    var getExecutive = document.getElementById('executiveEnter').value;
     var getCompany = document.getElementById("companyEnter").value; 
-    var getExecutive = document.getElementById('executiveEnter').value; 
     
-    var nameANDcompany = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20"+getCompany; 
-    var nameFacebookORTwitterORLinkedin = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20facebook%20OR%20twitter%20OR%20linkedin";
-    var nameANDcompanyJoinedORHiredORPromoted = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20"+getCompany+"%20joined%20OR%20hired%20OR%20promoted";
-    var nameANDcompanyJoinORHireORPromotion = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20"+getCompany+"%20join%20OR%20hire%20OR%20promotion";
-    var nameANDcompanysaidORcommentedORquoted = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20"+getCompany+"%20said%20OR%20commented%20OR%20quoted";
-    var nameANDcompanysayORcommentORquote = "http://www.google.com/search?q="+getExecutive+"+"+"AND%20"+getCompany+"%20say%20OR%20comment%20OR%20quote";
-    var getCompanyCeoANDCfoANDCoo = "http://www.google.com/search?q="+getCompany+"+"+"%20CEO%20AND%20CFO%20AND%20COO%20";
-    var getCompanyPresidentANDVicepresident = "http://www.google.com/search?q="+getCompany+"+"+"President%20AND%20Vice%20President";
+    var trimExe = getExecutive.trim(); 
+    var trimCom = getCompany.trim();  
+
+    var strExecutive = getExecutive.replace(/\s/g,'+'); 
+    var strCompany = getCompany.replace(/\s/g,'+');  
+
+    var nameANDcompany = "http://www.google.com/search?q="+strExecutive+"+"+"AND%20"+strCompany; 
+    var nameFacebookORTwitterORLinkedin = "http://www.google.com/search?q="+strExecutive+"+"+"AND%20facebook%20OR%20twitter%20OR%20linkedin";
+    var nameANDcompanyJoinedORHiredORPromoted = "http://www.google.com/search?q="+strExecutive+"+"+"AND%20"+strCompany+"%20joined%20OR%20hired%20OR%20promoted";
+    var nameANDcompanysaidORcommentedORquoted = "http://www.google.com/search?q="+strExecutive+"+"+"AND%20"+strCompany+"%20said%20OR%20commented%20OR%20quoted";
+    var getCompanyCeoANDCfoANDCoo = "http://www.google.com/search?q="+strCompany+"+"+"%20CEO%20AND%20CFO%20AND%20COO%20";
+    var getCompanyPresidentANDVicepresident = "http://www.google.com/search?q="+strCompany+"+"+"President%20AND%20Vice%20President";
     
-    document.querySelector(".links0").innerHTML = '<a target="_blank" href=' + nameANDcompany + '>' + getExecutive  + ' AND ' +  getCompany  + '</a>';    
+    document.querySelector(".links0").innerHTML = '<a target="_blank" href=' + nameANDcompany + '>' + trimExe + ' AND ' +  trimCom  + '</a>';    
     document.querySelector(".links1").innerHTML = '<a target="_blank" href=' + nameFacebookORTwitterORLinkedin + '>' + getExecutive + ' Facebook OR Linkedin OR Twitter'  + '</a>'; 
     document.querySelector(".links2").innerHTML = '<a target="_blank" href=' + nameANDcompanyJoinedORHiredORPromoted + '>' + getExecutive + ' AND ' + getCompany  + ' Joined OR Hired OR Promoted'  + '</a>';
     document.querySelector(".links3").innerHTML = '<a target="_blank" href=' + nameANDcompanysaidORcommentedORquoted + '>' + getExecutive + ' AND ' + getCompany  + ' Said OR Commented OR Quoted'  + '</a>';
